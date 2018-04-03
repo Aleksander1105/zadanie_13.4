80242a3ab0732ae0c6816a3ce8119313c349bef0
Aleksander1105 <matyjewiczaleksander@gmail.com>
2018-04-03 09:46:35 +0200

    Added some basic modifications

--- stuff.js
+++ stuff.js
@@ -1,4 +1,4 @@
-var OSinfo = require('/OSinfo');
+var wnInfo = require('../modules/wnInfo.js');
 
 process.stdin.setEncoding('utf-8');
 process.stdin.on('readable', function() {
@@ -17,7 +17,7 @@ process.stdin.on('readable', function() {
 				process.stdout.write('Process language is as following: ' + process.env.LANG + ' \n');
 				break;
 			case '/getOSinfo':
-				OSinfo.print();
+				wnInfo.print();
 				break;
 			default:
 				process.stderr.write('Wrong instruction!\n');
